308c5a6ed008e107ca35fbd3e1780ef9e742bdf5
cgl28 <Cameronlackey18@gmail.com>
2025-11-05 20:35:37 +0000

    UI midifications
    more UI modifications and small changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'                // your Tailwind file (keep it)
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: { main: '#1976d2' },
+    secondary: { main: '#6a1b9a' },
+  },
+  // optional: typography, component defaults/overrides
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+)
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
