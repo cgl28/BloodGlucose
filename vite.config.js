a63bee9c60ee6273e05aad5484542b8f4ae4e0f0
cgl28 <Cameronlackey18@gmail.com>
2025-11-06 10:45:51 +0000

    publish attempt
    sorting main / gh-pages branch

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // IMPORTANT: use your repo name here
-  base: '/bloodglucose/', 
+  base: '/BloodGlucose/', 
 })
